8032e55f38b0037de345546a289f0454bca965cb
nilerpadilla <nilerpaor08@gmail.com>
2025-11-22 14:24:52 -0500

    Delete src directory

--- src/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUserProfile } = require('../controllers/userController');
-const { authMiddleware } = require('../middleware/auth');
-
-router.get('/me', authMiddleware, (req, res) => res.json({ success: true, user: req.user }));
-router.get('/:id', authMiddleware, getUserProfile);
-
-module.exports = router;
